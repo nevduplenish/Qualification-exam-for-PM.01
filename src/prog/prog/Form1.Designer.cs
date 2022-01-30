fee3df50c7e5acc620e33f66990172ee2c9a6269
nevduplenish <vip.nick600@mail.ru>
2022-01-30 12:25:21 +0300

    Qualification-exam-for-PM.01
    Qualification-exam-for-PM.01

---
+++ src/prog/prog/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace prog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.информацияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 381);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(959, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(959, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.fileToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.информацияToolStripMenuItem});
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            // 
+            // информацияToolStripMenuItem
+            // 
+            this.информацияToolStripMenuItem.Name = "информацияToolStripMenuItem";
+            this.информацияToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.информацияToolStripMenuItem.Text = "Информация";
+            this.информацияToolStripMenuItem.Click += new System.EventHandler(this.infoToolStripMenuItem_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 27);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(947, 341);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 374);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(935, 10);
+            this.panel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(959, 403);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ToolStripMenuItem информацияToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ src/prog/prog/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prog
+{
+    public partial class Form1 : Form
+    {
+        // поля
+        private OpenFileDialog _openFile;
+
+        private Label _aboutCoordOne;
+        private Label _aboutCoordTwo;
+        
+        private TextBox _xCoordInput;
+        private TextBox _yCoordInput;
+        
+        private Button _calculateButton;
+
+        private double _xCoord;
+        private double _yCoord;
+
+        // инициализация компонентов
+        public Form1()
+        {
+            InitializeComponent();
+            _openFile = new OpenFileDialog();
+            _openFile.Filter = "Web page files (*.html)|*.html";
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            toolStripStatusLabel2.Text = "Программа запущена";
+        }
+
+        // метод для закрытия приложения
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // метод для открытия html файла 
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _openFile.ShowDialog();
+            webBrowser1.Navigate(_openFile.FileName);
+
+            // если файл был выбран
+            if (_openFile.FileName != String.Empty)
+            { 
+                ConfigureUI();  // изменение UI
+            }
+
+            toolStripStatusLabel2.Text = "Сатрница загружена";
+        }
+        // изменение UI
+        private void ConfigureUI()
+        {
+            this.Height = 600;
+            panel1.Height = 150;
+
+            _xCoordInput = new TextBox();
+            _yCoordInput = new TextBox();
+            _calculateButton = new Button();
+
+            _aboutCoordOne = new Label();
+            _aboutCoordTwo = new Label();
+
+            panel1.Controls.Add(_xCoordInput);
+            panel1.Controls.Add(_yCoordInput);
+            panel1.Controls.Add(_calculateButton);
+            panel1.Controls.Add(_aboutCoordOne);
+            panel1.Controls.Add(_aboutCoordTwo);
+
+            _aboutCoordOne.Text = "координата x";
+            _aboutCoordTwo.Text = "координата y";
+
+            _aboutCoordOne.Location = new Point(0, 25);
+            _aboutCoordTwo.Location = new Point(0, 50);
+
+            _xCoordInput.Location = new Point(75, 25);
+            _yCoordInput.Location = new Point(75, 50);
+
+            _calculateButton.Text = "определить";
+            _calculateButton.Location = new Point(0, 75);
+            _calculateButton.Click += _calculateButton_Click;
+
+        }
+        // мето для вычисления длины точки отностиельно центра окружности тестовой области
+        private double CalcLength(double x,double y)
+        {
+            return Math.Sqrt((x - 1) * (x - 1) + (y - 0) * (y - 0));
+        }
+        // метод активирующийся при клике на кнопку для определения принадлежности точки 
+        private void _calculateButton_Click(object sender, EventArgs e)
+        {
+            // если введённые данные преобразуемы в координаты точек
+            if (double.TryParse(_xCoordInput.Text, out _xCoord) && double.TryParse(_yCoordInput.Text, out _yCoord))
+            {
+                // если точка принадлежит области
+                if ((_yCoord <= -(_xCoord - 3)) && _yCoord >= (_xCoord - 3) && CalcLength(_xCoord, _yCoord) <= 2)
+                {
+                    // если точка принадлежит границе области
+                    if (CalcLength(_xCoord, _yCoord) == 2 || _yCoord == -(_xCoord - 3) || _yCoord == (_xCoord - 3))
+                    {
+                        toolStripStatusLabel2.Text = "Точка лежит на границе";
+                        MessageBox.Show("Точка лежит на границе");
+                        return;
+                    }
+
+                    toolStripStatusLabel2.Text = "Точка входит в область";
+                    MessageBox.Show("Точка входит в область");
+                }
+                // иначе точка не принадлдежит области
+                else
+                {
+                    toolStripStatusLabel2.Text = "Точка не входит в область";
+                    MessageBox.Show("Точка не входит в область");
+                }
+            }
+            // иначе введённые данные не преобразуемы в координаты точки
+            else
+            {
+                toolStripStatusLabel2.Text = "Некорректный ввод";
+            }
+        }
+        
+        // метод длявывода данных о создателе
+        private void infoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Создатель: Рогожин Денис ПКсп-119 Вариант 7");
+        }
+
+    }
+}
